b221c5aeb914ad0fa28e40745ec59c19d35189f0
suneelbelkhale <neelus.genius@gmail.com>
2019-10-18 15:03:07 -0700

    fixed data analysis script

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
 from scipy.io import loadmat
-from utils import get_weights
 
 
 # plots the following:
@@ -35,6 +34,8 @@ if __name__ == "__main__":
     ac_dist = dct['ac_dist']
     episode_sizes = dct['episode_sizes']
 
+    # dist = np.random.normal()
+
     # conditional action histograms (3x3 for pendulum)
 
     sorted_dims = np.argsort(obs, axis=0)
@@ -42,25 +43,27 @@ if __name__ == "__main__":
     obs_ranges = np.amax(obs, axis=0) - np.amin(obs, axis=0)
     bin_obs_ranges = obs_ranges / args.nb
 
+    last_range = np.amin(obs, axis=0)
+
     for bn in range(args.nb):
         fi, axsi = plt.subplots(obs.shape[1], acs.shape[1], tight_layout=True, figsize=(3 * obs.shape[1], 3 * acs.shape[1]))
-        fi.suptitle()
-        for obdim in range(obs.shape[0]):
-            order = sorted_dims[:, obdim]
-            for acdim in range(asc.shape[0]):
-                axsi[obdim, acdim].hist(acs[:, acdim])
-                axsi[obdim, acdim].hist(acs[:, acdim])
+        next_range = last_range + bin_obs_ranges
+        fi.suptitle("Range %s -> %s" % (str(last_range), str(next_range)))
+        for obdim in range(obs.shape[1]):
+            idx_range = sorted_dims[bn * bin_size : (bn+1) * bin_size, obdim]
+            for acdim in range(acs.shape[1]):
+                axsi[obdim, acdim].hist(acs[idx_range, acdim])
+                axsi[obdim, acdim].hist(acs[idx_range, acdim])
+                # axsi[obdim, acdim].plot(acs[:, acdim])
+        last_range = next_range
 
-
-
-    f2, axs2 = plt.subplots(3, 1, tight_layout=True, figsize=(10,10))
+    f2, axs2 = plt.subplots(1 + acs.shape[1], 1, tight_layout=True, figsize=(10,10))
     # first plot rollouts
     axs2[0].set_title("Rollout Lengths")
     axs2[0].hist(episode_sizes)
-    axs2[1].set_title("Rollout Total Costs")
-    axs2[1].hist()
-    axs2[2].set_title("Rollout Final Costs")
-    axs2[2].hist(final_costs)
+    for i in range(acs.shape[1]):
+        axs2[1 + i].set_title("Action dist (dim %d)" % i)
+        axs2[1 + i].hist(acs[:, i])
 
     if args.savefolder != '':
         suff = "k_%d.png" % (args.k)
